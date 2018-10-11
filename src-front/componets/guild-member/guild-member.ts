a8af392d6f522a900f683659523dc38e794812da
Jessica DeVriese <jessdev@umich.edu>
2018-10-10 22:35:00 -0400

    moved image to app folder

--- src-front/componets/guild-member/guild-member.ts
+++ src-front/componets/guild-member/guild-member.ts
@@ -1,5 +1,13 @@
+import { bindable, bindingMode } from 'aurelia-framework';
+
 export class GuildMember{
-    constructor(){
-        
+    @bindable public imagepath: string | undefined;
+    @bindable public description: string | undefined;
+    @bindable public quote: string | undefined;
+
+    constructor(imagepath: string, description: string, quote: string){
+        this.imagepath = imagepath;
+        this.description = description;
+        this.quote = quote;
     }
 }
--- src-front/routes/members.ts
+++ src-front/routes/members.ts
@@ -1,5 +1,12 @@
+ import { GuildMember } from '../componets/guild-member/guild-member';
+
 export class Members{
+    public memberList: GuildMember[];
     constructor(){
-        
+        this.memberList = [
+            new GuildMember('images/thisisur-thumb.png', //'../assets/guild-members/thisisur-thumb.png', 
+            "Thisisur is our resident Blood Elf Paladin. He's okay at his job", 
+            'Thisisur Paladin speaking, please keep your hands and feet inside the tank at all times')
+        ];
     }
 }
