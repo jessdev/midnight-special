e8e4796a42a3b2eb0bc0ecbf1378357fa94eaa30
Jessica DeVriese <jessdev@umich.edu>
2018-10-12 08:11:56 -0400

    added another member

--- src-front/routes/members.ts
+++ src-front/routes/members.ts
@@ -4,9 +4,12 @@ export class Members{
     public memberList: GuildMember[];
     constructor(){
         this.memberList = [
-            new GuildMember('images/thisisur-thumb.png', //'../assets/guild-members/thisisur-thumb.png', 
+            new GuildMember('images/thisisur-thumb.png',
             "Thisisur is our resident Blood Elf Paladin. He's okay at his job", 
-            '"Thisisur Paladin speaking, please keep your hands and feet inside the tank at all times"')
+            '"Thisisur Paladin speaking, please keep your hands and feet inside the tank at all times"'),
+            new GuildMember('images/cerules-thumb.png',
+            "Placeholder Text", 
+            '"Placeholder Text"')
         ];
     }
 }
