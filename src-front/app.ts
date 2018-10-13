99c2a3720480ca7cf34efd0e51ddd81eec77e621
Jessica DeVriese <jessdev@umich.edu>
2018-10-13 15:59:52 -0400

    got the css to actually work

--- src-front/app.ts
+++ src-front/app.ts
@@ -1,4 +1,4 @@
-import {Aurelia} from 'aurelia-framework';
+import {Aurelia, computedFrom} from 'aurelia-framework';
 import {Router, RouterConfiguration} from 'aurelia-router';
 import {PLATFORM} from 'aurelia-pal';
 import 'bootstrap';
@@ -22,4 +22,12 @@ export class App {
 
     this.router = router;
   }
+
+    @computedFrom('router.currentInstruction.config.name')
+    get isHome(): boolean {
+      console.log(this.router!.currentInstruction.config.name);
+      console.log(this.router!.currentInstruction.config.name === 'home');
+      return (this.router!.currentInstruction.config.name === 'home');
+    }
+  
 }
