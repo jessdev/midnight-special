daf1e4dde19a98d403170e8e105a9704e6cd6fd1
Jessica DeVriese <jessdev@umich.edu>
2018-10-15 20:40:55 -0400

    updated about

--- src-front/routes/about.ts
+++ src-front/routes/about.ts
@@ -9,7 +9,7 @@ export class About {
             new GuildMember('Thisisur',
             'Guild Social Chair - Tank',
             'images/thisisur-thumb.png',
-            "Thisisur is our resident Blood Elf Paladin. He's okay at his job", 
+            "Thisisur is our resident Blood Elf Paladin. Currently the off tank in our raid team, this is his first time on the front line of the expansion.", 
             '"Thisisur Paladin speaking, please keep your hands and feet inside the tank at all times"',
             "Blood Elf",
             "Paladin"
@@ -17,8 +17,8 @@ export class About {
             new GuildMember('Cerules',
             'Founder - Healer',
             'images/cerules-thumb.png',
-            "Placeholder Text", 
-            '"Placeholder Text"',
+            "Cerules is a veteran of the front lines of Azeroth. He's done a tour of Pandaria and heals for a living as a priest.", 
+            '"Oh.... I used halo again."',
             "Forsaken",
             "Priest"
             )
